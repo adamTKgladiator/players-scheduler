ed530d769107cda156ebd7fa2f76c6605988ce3f
adamTKgladiator <adamkaak48@gmail.com>
2025-06-22 00:08:59 +0100

    Update list.js

--- pages/api/list.js
+++ pages/api/list.js
@@ -1,5 +1,9 @@
 import { Redis } from '@upstash/redis';
-const redis = Redis.fromEnv();
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
 
 export default async function handler(req, res) {
   const list = await redis.lrange('entries', 0, 1000);
