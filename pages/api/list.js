280fc63bdf9fae2476a003b09e6c9638de235a90
adamTKgladiator <adamkaak48@gmail.com>
2025-06-21 23:59:04 +0100

    Create list.js

---
+++ pages/api/list.js
@@ -0,0 +1,8 @@
+import { Redis } from 'upstash-redis';
+const redis = Redis.fromEnv();
+
+export default async function handler(req, res) {
+  const list = await redis.lrange('entries', 0, 1000);
+  const data = list.map(JSON.parse);
+  res.status(200).json(data);
+}
